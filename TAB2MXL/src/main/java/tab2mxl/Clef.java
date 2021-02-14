0ca712d1df36b03dd148f0b30fbe66b1cbf656ba
Hashir <hashir.jamil17@gmail.com>
2021-02-14 14:53:35 -0500

    updated XML classes for marshalling

--- TAB2MXL/src/main/java/tab2mxl/Clef.java
+++ TAB2MXL/src/main/java/tab2mxl/Clef.java
@@ -3,4 +3,23 @@ package tab2mxl;
 public class Clef {
 	char sign;
 	int line;
+	
+	public Clef(char sign, int line) {
+		super();
+		this.sign = sign;
+		this.line = line;
+	}
+	
+	public char getSign() {
+		return sign;
+	}
+	public void setSign(char sign) {
+		this.sign = sign;
+	}
+	public int getLine() {
+		return line;
+	}
+	public void setLine(int line) {
+		this.line = line;
+	}
 }
--- TAB2MXL/src/main/java/tab2mxl/Pitch.java
+++ TAB2MXL/src/main/java/tab2mxl/Pitch.java
@@ -3,4 +3,27 @@ package tab2mxl;
 public class Pitch {
 	int step;
 	int octave;
+	
+	public Pitch(int step, int octave) {
+		super();
+		this.step = step;
+		this.octave = octave;
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
+	}
+
+	public int getOctave() {
+		return octave;
+	}
+
+	public void setOctave(int octave) {
+		this.octave = octave;
+	}
+	
 }
