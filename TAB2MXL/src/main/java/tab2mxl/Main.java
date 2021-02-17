f96c5c16a9af78c211a6fc3a8e77ea366a4ae54c
Niharika Rajnish <niharikarajnish49@gmail.com>
2021-02-16 22:47:30 -0500

    gui

--- TAB2MXL/src/main/java/tab2mxl/Main.java
+++ TAB2MXL/src/main/java/tab2mxl/Main.java
@@ -1,56 +1,51 @@
 package tab2mxl;
 
+import java.io.File;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.control.Label;
-import java.util.Optional;
-
-
-import java.io.File;
-import javafx.application.Application;
-import javafx.stage.DirectoryChooser;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.Text;
-import javafx.collections.ObservableList;
-import javafx.geometry.VPos;
-import javafx.scene.layout.StackPane; 
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-
-import javafx.scene.control.TextInputDialog;
-import javafx.event.EventHandler;
-
-import javafx.event.ActionEvent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.Group;
-import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.FileChooser.ExtensionFilter;
 
-public class Main extends Application {
-
+public class Main extends Application{
 	
-	Group group = new Group();
+	Stage window;
+	Scene scene1,scene2;
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-	public void createText(){
-		TextFlow textFlow = new TextFlow();
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		window = primaryStage;
+		
+		 Image background_image = new Image("file:soundwave.jpg");
+	     ImageView mv = new ImageView(background_image);
+	     mv.setFitHeight(1000);
+	     mv.setFitWidth(1000);
+	     mv.setOpacity(1);
+	     
+	     TextFlow textFlow = new TextFlow();
 		 Text title = new Text("Zebra11 Converter \n");
 		 title.setFill(Color.WHITE);
 		  title.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
@@ -58,103 +53,130 @@ public class Main extends Application {
 	        textFlow.setLayoutX(250);
 	        textFlow.setLayoutY(50);
 	        
-	        group.getChildren().add(textFlow);
+	        TextFlow textFlow3 = new TextFlow();
+	        Font start_font = new Font("Tahoma", 30);
+	        Text start = new Text("Start!");
+	        start.setFill(Color.WHITE);
+	        start.setFont(start_font);
+	        textFlow3.getChildren().add(start);
+	        textFlow3.setLayoutX(470);
+	        textFlow3.setLayoutY(350);
+	        
+	      
 	        
 	        
-	        TextFlow textFlow2 = new TextFlow();
-	        Font instructions_font = new Font("Tahoma", 20);
-	        Text instuctions = new Text("Welcome to Zebra11 tablature to MusicXML converter! You can choose to convert a Tablature file \n or simply input the tabs in the text box below and click enter.");
-	        instuctions.setFill(Color.WHITE);
-	        instuctions.setFont(instructions_font);
-	        textFlow2.getChildren().add(instuctions);
-	        textFlow2.setLayoutX(100);
-	        textFlow2.setLayoutY(150);
+	        final Button button = new Button();
+	        button.setShape(new Circle(1));
+	        button.setMaxSize(3,3);
+
+	        ImageView icon = new ImageView("file:icon1.png");
+	        icon.setFitHeight(150);
+	        icon.setFitWidth(150);
+	        button.setGraphic(icon);
 	        
-	        group.getChildren().add(textFlow2);
 
-	}
+	        button.setLayoutX(420);
+	        button.setLayoutY(400);
+	        button.setOnAction(e -> window.setScene(scene2));
 	
+	        
+	        Pane layout1 = new Pane();
+	        layout1.setPrefSize(1000,1000);
+	        layout1.getChildren().add(mv);
+	        layout1.getChildren().add(textFlow);
+	        layout1.getChildren().add(button);
+	        layout1.getChildren().add(textFlow3);
+	      
+	        
+	        Scene scene1 = new Scene(layout1, 1000, 1000, Color.BLACK);
 
-     
-     //create set on action for this button
+	     
+	     //2nd page
+	        
+	        Image background_image2 = new Image("file:soundwave.jpg");
+		     ImageView mv2 = new ImageView(background_image);
+		     mv2.setFitHeight(1000);
+		     mv2.setFitWidth(1000);
+		     mv2.setOpacity(1);
+		     
+		     TextFlow textFlow2 = new TextFlow();
+			 Text title2 = new Text("Zebra11 Converter \n");
+			 title2.setFill(Color.WHITE);
+			  title2.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+		        textFlow2.getChildren().add(title2);
+		        textFlow2.setLayoutX(250);
+		        textFlow2.setLayoutY(50);
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		
-		
-		
-		createText();
+		        TextFlow textFlow4 = new TextFlow();
+		        Font instructions_font = new Font("Tahoma", 20);
+		        Text instuctions = new Text("Welcome to Zebra11 tablature to MusicXML converter! You can choose to convert a Tablature file \n or simply input the tabs in the text box below and click enter.");
+		        instuctions.setFill(Color.WHITE);
+		        instuctions.setFont(instructions_font);
+		        textFlow4.getChildren().add(instuctions);
+		        textFlow4.setLayoutX(100);
+		        textFlow4.setLayoutY(150);
+		        
+		        
+		        FileChooser fileChooser = new FileChooser();
+		        fileChooser.setTitle("Open Resource File");
+		        fileChooser.getExtensionFilters().add(new ExtensionFilter("Text Files", "*.txt"));
+		        final Button openButton = new Button("Choose a Tablature File");
+		   
+		        openButton.setLayoutX(400);
+		        openButton.setLayoutY(500);
+		        openButton.setMinSize(200, 100);
+		        openButton.setOnAction(
+		                new EventHandler<ActionEvent>() {
+		                    @Override
+		                    public void handle(final ActionEvent e) {
+		                       File file = fileChooser.showOpenDialog(primaryStage);
+		                        if (file != null) {
+		                        	System.out.print(file.getPath());
+		                        }
+		                    }
+		                });
+		        
+		        TextField textbox = new TextField("input tab here"); 
+		        textbox.setLayoutX(150);
+		        textbox.setLayoutY(250);
+		        textbox.setMinSize(700, 200);
+		        
+		        
+		        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
+		            public void handle(ActionEvent e) 
+		            { 
+		                System.out.print(textbox.getText());   
+		                textbox.clear();
+		                
+		            } 
+		        }; 
+		        
+		        textbox.setOnAction(event); 
+		     
+		  
+		        Pane layout2 = new Pane();
+		        layout2.setPrefSize(1000,1000);
+		        layout2.getChildren().add(mv2);
+		        layout2.getChildren().add(textFlow2);
+		        layout2.getChildren().add(textFlow4);
+		        layout2.getChildren().add(openButton);
+		        layout2.getChildren().add(textbox);
+		        
+		        
 	
+		     
+		scene2 = new Scene(layout2, 1000, 1000,Color.BLACK);
 		
-        
-        Image background_image = new Image("file:soundwave.jpg");
-        ImageView mv = new ImageView(background_image);
-        mv.setFitHeight(1000);
-        mv.setFitWidth(1000);
-        mv.setOpacity(0.4);
-        
-        group.getChildren().add(mv);
-        
-        
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
-        final Button openButton = new Button("Choose a Tablature File");
-   
-        openButton.setLayoutX(400);
-        openButton.setLayoutY(500);
-        openButton.setMinSize(200, 100);
-        openButton.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(final ActionEvent e) {
-                       File file = fileChooser.showOpenDialog(stage);
-                        if (file != null) {
-                            System.out.print("file was chosen");
-                        }
-                    }
-                });
-        
-      
-        
-        group.getChildren().add(openButton);
-        
-        TextField b = new TextField("input tab here"); 
-        b.setLayoutX(150);
-        b.setLayoutY(250);
-        b.setMinSize(700, 200);
-        
-        
-       // TilePane r = new TilePane(); 
-        Label l = new Label("no text");
-        
-        
-     // action event 
-        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
-            public void handle(ActionEvent e) 
-            { 
-                System.out.print(b.getText());   
-                b.clear();
-                
-            } 
-        }; 
-  
-        // when enter is pressed 
-        b.setOnAction(event); 
-  
-        // add textfield 
-        group.getChildren().add(b); 
-        group.getChildren().add(l); 
-  
-      
-        Scene scene = new Scene(group, 1000, 1000, Color.BLACK);
-        stage.setTitle("Tablature to MusicXML Converter");
-        stage.setScene(scene);
-
-        stage.show();
+		window.setScene(scene1);
+		window.setTitle("Tablature to MusicXML Converter");
+		window.show();
+		
+		
+		
+		
+		
+		
 		
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
 }
