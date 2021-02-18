8a24fbaac08e60b5e811f80513560a5e534b46ea
Tommy Lam <tommy16@my.yorku.ca>
2021-02-17 19:57:22 -0500

    Tommy's push

--- TAB2MXL/src/main/java/tab2mxl/Step.java
+++ TAB2MXL/src/main/java/tab2mxl/Step.java
@@ -1,6 +1,7 @@
 package tab2mxl;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /*
  * @Author Tommy Lam, Hashir Jamil
@@ -8,25 +9,52 @@ import java.util.ArrayList;
 
 public class Step {
 
-	public void getInfoFromArray(char[][] array) {
+	
+	public static int counter = 0;
+	
+	public ArrayList<Note> getInfoFromArray(char[][] array) {
 
 
-		ArrayList<Integer> Notes = new ArrayList<Integer>();
+		//List<Integer> Notes = new ArrayList<Integer>();
+		List<Note> NoteList = new ArrayList<Note>();
 
 
 		for (int i = 0; i<array.length; i++)
-			for(char j = 0; i <= 20; i++) {
+			for(int j = 0; j < array[i].length; j++) {
 
 				int a = Character.getNumericValue(array[j][i]);
 				//if you encounter a note
-				if(0 <= a &&  a <= 100) {
-					int x = array[j][i];
-					Notes.add(x);
-
+				if(0 <= a && a <= 9) {
+					int s = i;
+					int f = array[j][i];
+					Note n = new Note(s,f);
+					NoteList.add(n);
 				}
 
 
 			}
+		return (ArrayList<Note>) NoteList;
 	}
+	
+	public int duration(char [][] array) {
+		
+		
+		for(int i = 0; i<array[i].length; i++) {
+			for(int j =0; j < array[i].length; j++) {
+				
+				int a = Character.getNumericValue(array[j][i]);
+				if(0 <= a && a <= 9) {
+					int b = Character.getNumericValue(array[j][i]);
+					if(b==-1) {
+						for(int j = 0; j)
+					}
+				}
+				
+			}
+		}
+		
+		return counter;
+	}
+	
 
 }
