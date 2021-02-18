0eed03a278bb64b16d52e8f259a4189839f28602
Tommy Lam <tommy16@my.yorku.ca>
2021-02-17 22:45:44 -0500

    Added Note List Method to Measure

--- TAB2MXL/src/main/java/tab2mxl/Measure.java
+++ TAB2MXL/src/main/java/tab2mxl/Measure.java
@@ -58,6 +58,30 @@ public class Measure {
 		barline = new Barline();
 		
 	}
+	
+	public ArrayList<Note> getInfoFromArray(char[][] array) {
+
+
+		//List<Integer> Notes = new ArrayList<Integer>();
+		List<Note> NoteList = new ArrayList<Note>();
+
+
+		for (int i = 0; i<array.length; i++)
+			for(int j = 0; j < array[i].length; j++) {
+
+				int a = Character.getNumericValue(array[j][i]);
+				//if you encounter a note
+				if(0 <= a && a <= 9) {
+					int s = i;
+					int f = array[j][i];
+					Note n = new Note(s,f);
+					NoteList.add(n);
+				}
+
+
+			}
+		return (ArrayList<Note>) NoteList;
+	}
 
 
 
