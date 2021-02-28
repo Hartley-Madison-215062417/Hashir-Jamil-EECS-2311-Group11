aca92341f0ade61f9267dd9c51d1ced9747c1e5a
Hartley-Madison-215062417 <maddi15@my.yorku.ca>
2021-02-28 15:52:00 -0500

    The start of JUnit integration

---
+++ TAB2MXL/src/main/java/testers/ParserJUnit.java
@@ -0,0 +1,30 @@
+package testers;
+
+import tab2mxl.Parser;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/*
+ * @Author: Madison Hartley
+ */
+
+
+class ParserJUnit {
+	
+	
+	private Parser p;
+	
+	@BeforeEach
+	public void setup() throws Exception {
+		p = new Parser();
+	}
+	
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
