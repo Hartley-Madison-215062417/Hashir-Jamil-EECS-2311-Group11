59dbfa44695034ca945db2e12a2a966c8180e783
Tommy Lam <tommy16@my.yorku.ca>
2021-02-28 01:53:51 -0500

    Completed Drums Voice

---
+++ TAB2MXL/src/main/java/tab2mxl/DrumsVoice.java
@@ -0,0 +1,20 @@
+package tab2mxl;
+
+/*
+ * @Author Tommy Lam
+ */
+
+public class DrumsVoice {
+	
+	int x;
+	public int voice(String id) {
+		if(id == "P1-I36") {
+			x = 2;
+		}else {
+			x = 1;
+		}
+		return x;
+		
+	}
+
+}
