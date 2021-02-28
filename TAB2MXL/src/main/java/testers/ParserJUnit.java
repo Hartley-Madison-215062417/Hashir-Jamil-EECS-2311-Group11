5c873e72ea8c3d83490b3b8cadbaa63f1173246c
Hartley-Madison-215062417 <maddi15@my.yorku.ca>
2021-02-28 16:39:48 -0500

    Continuing JUnit, pushing and committing now incase backup needed for next part

--- TAB2MXL/src/main/java/testers/ParserJUnit.java
+++ TAB2MXL/src/main/java/testers/ParserJUnit.java
@@ -3,6 +3,8 @@ package testers;
 import tab2mxl.Parser;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,16 +17,30 @@ class ParserJUnit {
 	
 	
 	private Parser p;
+	private Parser q;
+	private File file = new File("prototypeGuitarTab.txt");
 	
 	@BeforeEach
 	public void setup() throws Exception {
-		p = new Parser();
+		p = new Parser("prototypeGuitarTab");
+		q = new Parser(file);
 	}
 	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructors() {
+		
+		assertEquals(p.getTabCharMatrix()[1],q.getTabCharMatrix()[1]);
+		assertEquals(q.getTabCharMatrix()[1],p.getTabCharMatrix()[1]);
+		
 	}
+	
+	
+	public void testGetFirstLine() {
+		
+		
+		
+	}
+	
 
 }
