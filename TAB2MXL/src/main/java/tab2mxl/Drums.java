d29873f9eaa612a2f8b42f5ee348243e0c57cca1
Tommy Lam <tommy16@my.yorku.ca>
2021-02-27 21:21:58 -0500

    Start of Drums parser

---
+++ TAB2MXL/src/main/java/tab2mxl/Drums.java
@@ -0,0 +1,23 @@
+package tab2mxl;
+
+public class Drums {
+	
+	public String instrumentID(String instrument) {
+		String instID = " ";
+		if (instrument == "CC || C"){
+				instID = "Crash Symbal";
+		}else if(instrument == "R || Rd") {
+			instID = "Ride Cymbal";
+		}else if(instrument == "HH || H") {
+			instID = "P1-I43";
+		}else if(instrument == "HF") {
+			instrument = "Closed Hi-Hat";
+		}
+		
+
+		return inst;
+	}
+	
+	
+
+}
