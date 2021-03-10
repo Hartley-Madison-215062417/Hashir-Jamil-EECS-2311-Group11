31b5b7826bea2e55ec21c09a8d6f6721c71186d2
Tommy Lam <tommy16@my.yorku.ca>
2021-03-10 01:03:55 -0500

    scorePart added

--- TAB2MXL/src/main/java/drums/Note.java
+++ TAB2MXL/src/main/java/drums/Note.java
@@ -11,35 +11,35 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(propOrder= {"unpitched","duration","instrumentID","voice","type","stem","notehead","beamnumber"})
 
 public class Note{
-	
+
 	@XmlElement
 	Unpitched unpitched = new Unpitched();
-	
+
 	@XmlElement
 	int duration;
-	
+
 	@XmlElement
 	String instrumentID;
-	
+
 	@XmlElement
 	int voice;
-	
+
 	@XmlElement
 	String type;
-	
+
 	@XmlElement
 	String stem;
-	
+
 	@XmlElement
 	String notehead;
-	
+
 	@XmlElement
 	String beamnumber;
-	
+
 	public Note() {
 		super();
 	}
-	
+
 	public Note(Unpitched unpitched, int duration, String instrumentID, int voice, String type, String stem, String notehead, String beamnumber) {
 		super();
 		this.unpitched = unpitched;
@@ -50,77 +50,77 @@ public class Note{
 		this.stem = stem;
 		this.notehead = notehead;
 		this.beamnumber = beamnumber;
-		
-		
+
+
 	}
-	
+
 	public Unpitched getUnpitched() {
 		return unpitched;
 	}
-	
+
 	public void setUnpitched(Unpitched unpitched) {
 		this.unpitched = unpitched;
 	}
-	
+
 	public int getDuration() {
 		return duration;
 	}
-	
+
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
-	
+
 	public String setInstrumentID() {
 		return instrumentID;
 	}
-	
+
 	public void getInstrumentID(String instrumentID) {
 		this.instrumentID = instrumentID;
 	}
-	
+
 	public int getVoice() {
 		return voice;
 	}
-	
+
 	public void setVoice(int voice) {
 		this.voice = voice;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
-	
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	
+
 	public String getStem() {
 		return stem;
 	}
-	
+
 	public void setStem(String stem) {
 		this.stem = stem;
 	}
-	
+
 	public String getNotehead() {
 		return notehead;
 	}
-	
+
 	public void setNotehead(String notehead) {
 		this.notehead = notehead;
 	}
-	
+
 	public String getBeamnumber() {
 		return beamnumber;
 	}
-	
+
 	public void setBeamnumber(String beamnumber) {
 		this.beamnumber = beamnumber;
 	}
 
 	public void setOctave(Note n) {
 		int voice = n.getVoice();
-		
+
 		if(voice == 1) {
 			n.getUnpitched().setOctave(5);
 		}else if(voice == 2) {
@@ -128,6 +128,6 @@ public class Note{
 		}else {
 			n.getUnpitched().setOctave(0);
 		}
-		}
 	}
+}
 
