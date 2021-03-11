b5fc6974cbcdef98765163f7300549262b69f476
Abhirami Venugopal <abhirami@my.yorku.ca>
2021-03-11 06:52:51 -0500

    Minor changes

--- TAB2MXL/src/main/java/guitar/Main.java
+++ TAB2MXL/src/main/java/guitar/Main.java
@@ -12,34 +12,34 @@ class Main{
 	public static void main(String[] args) {
 	try {
 		
-		Parser p = new Parser("prototypeGuitarTab.txt");
-        char[][] parsed = p.getTabCharMatrix();
-        
-        ArrayList<char[][]> testArrayList3 = new ArrayList<char[][]>();
-        testArrayList3.add(parsed);
-        testArrayList3.add(parsed);
-        
-        char[][] tmp = testArrayList3.get(0);
-        
-        for (int i = 0; i < tmp.length ; i++) {
-            System.out.println(p.getTabCharMatrix()[i]);
-        }
-        
-        char[][] tmp2 = testArrayList3.get(1);
-        for (int i = 0; i < tmp2.length ; i++) {
-            System.out.println(p.getTabCharMatrix()[i]);
-        }
-		
-		Parser parser = new Parser("prototypeGuitarTab.txt");
-		Part part = parser.createMusicalPart(testArrayList3);
-		
-		JAXBContext jc = JAXBContext.newInstance(Part.class);
-		Marshaller ms = jc.createMarshaller();
-		ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		ms.marshal(part, System.out);
-		ms.marshal(part,new File("src//output//Output.xml"));
-        
-        
+		Parser p = new Parser("src/main/java/prototypeGuitarTab.txt");
+//        char[][] parsed = p.getTabCharMatrix();
+//        
+//        ArrayList<char[][]> testArrayList3 = new ArrayList<char[][]>();
+//        testArrayList3.add(parsed);
+//        testArrayList3.add(parsed);
+//        
+//        char[][] tmp = testArrayList3.get(0);
+//        
+//        for (int i = 0; i < tmp.length ; i++) {
+//            System.out.println(p.getTabCharMatrix()[i]);
+//        }
+//        
+//        char[][] tmp2 = testArrayList3.get(1);
+//        for (int i = 0; i < tmp2.length ; i++) {
+//            System.out.println(p.getTabCharMatrix()[i]);
+//        }
+//		
+//		Parser parser = new Parser("src/main/java/guitar/prototypeGuitarTab.txt");
+//		Part part = parser.createMusicalPart(testArrayList3);
+//		
+//		JAXBContext jc = JAXBContext.newInstance(Part.class);
+//		Marshaller ms = jc.createMarshaller();
+//		ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//		ms.marshal(part, System.out);
+//		ms.marshal(part,new File("src//output//Output.xml"));
+//        
+//        
 
 		
 	}catch (JAXBException e) {
