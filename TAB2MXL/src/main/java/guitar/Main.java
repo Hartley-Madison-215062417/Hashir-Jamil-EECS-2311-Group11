568a869bebf251a2b493ebd647c7f724c334eb66
Abhirami Venugopal <abhirami@my.yorku.ca>
2021-03-15 12:57:11 -0400

    testing out measureSplitter to make it work for repeated measures

--- TAB2MXL/src/main/java/guitar/Main.java
+++ TAB2MXL/src/main/java/guitar/Main.java
@@ -1,62 +1,98 @@
 package guitar;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+/*
+ * @Author: Madison Hartley
+ */
+public class Main {
 
-class Main{
-	
 	public static void main(String[] args) {
-	try {
 		
-//		Parser p = new Parser("prototypeGuitarTab.txt");
-//        char[][] parsed = p.getTabCharMatrix();
-//        
-//        ArrayList<char[][]> testArrayList3 = new ArrayList<char[][]>();
-//        testArrayList3.add(parsed);
-//        testArrayList3.add(parsed);
-//        
-//        char[][] tmp = testArrayList3.get(0);
-//        
-//        for (int i = 0; i < tmp.length ; i++) {
-//           // System.out.println(p.getTabCharMatrix()[i]);
-//        }
-//        
-//        char[][] tmp2 = testArrayList3.get(1);
-//        for (int i = 0; i < tmp2.length ; i++) {
-//           // System.out.println(p.getTabCharMatrix()[i]);
-//        }
-//		
-//		Parser parser = new Parser("prototypeGuitarTab.txt");
-//		Part part = parser.createMusicalPart(testArrayList3);
-//		scorePartwise sp = new scorePartwise();
-//		sp.getParts().add(part);
-//		
-//		
-//		
-//		
-//		JAXBContext jc = JAXBContext.newInstance(scorePartwise.class);
-//		Marshaller ms = jc.createMarshaller();
-//		ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//		ms.marshal(sp, System.out);
-//		ms.marshal(sp,new File("src//output//Output.xml"));
+		Parser p = new Parser("creep.txt");
+		char[][] parsed = p.getTabCharMatrix();
 		
-		hammerOns h = new hammerOns();
-		JAXBContext jc = JAXBContext.newInstance(hammerOns.class);
-		Marshaller ms = jc.createMarshaller();
-		ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		ms.marshal(h, System.out);    
+		int width = parsed[0].length; //gets the length of the entire first row
+		
+		ArrayList<char[][]>tmpArray1 = p.measureSplitter(parsed);
+		char[][] tmpPrint1 = tmpArray1.get(0);
+		
+//		System.out.println();
+//		System.out.println("Printing only first measure stored in array list: ");
+//		System.out.println();
+//		
+//		
+//		/*
+//		 * This will print the first measure in the array list.
+//		 */
+//		char[][] tmpPrint1 = tmpArray1.get(0);
+//		for(int j = 0; j < tmpPrint1.length; j++) {
+//			System.out.println(" ");
+//			for(int k = 0; k < tmpPrint1[0].length; k++) {
+//				System.out.print(tmpPrint1[j][k]);
+//			}
+//			if(j+1 == tmpPrint1.length) {
+//				System.out.println(" ");
+//			}
+//		}
+//		
+//		System.out.println();
+//		System.out.println("Printing every measure stored in array list: ");
+//		System.out.println(" ");
+//		
+//		/*
+//		 * This will print every element in the array list.
+//		 */
+//		
+//		for(int i = 0; i < tmpArray1.size(); i++) {
+//			char[][] tmpPrint2 = tmpArray1.get(i);
+//			for(int j = 0; j < tmpPrint2.length; j++) {
+//				System.out.println(" ");
+//				for(int k = 0; k < tmpPrint2[0].length; k++) {
+//					System.out.print(tmpPrint2[j][k]);
+//				}
+//			}
+//			System.out.println(" ");
+//		}
+//		
+//	/*
+//	 * THE METHOD STARTS HERE	
+//	 */
+//		ArrayList<char[][]> tmpArray = new ArrayList<char[][]>();
+//		int newRow = 0;
+//		int newCol = 0;
+//		int counter = 0;
+//		int newColEnd = 0;
+//
+//		
+//		for(int i = 0; i < width; i++) {
+//			if(parsed[0][i] != '|') {
+//			}
+//			else {
+//				counter++;
+//				if(counter == 1) {
+//					newCol = i+1;
+//				}
+//				if(counter > 1) {
+//					newColEnd = i;
+//					char[][] newMeasure = new char[6][newColEnd];
+//					while(newCol < newColEnd) {
+//						for(newRow = 0; newRow < 6; newRow++) {
+//							newMeasure[newRow][newCol] = parsed[newRow][newCol];
+//						}
+//						newCol++;
+//					}
+//					
+//					tmpArray.add(newMeasure);
+//					
+//				}
+//			}
+//			
+//			
+//			
+//		}
+		
 
-		
-	}catch (JAXBException e) {
-		// TODO Auto-generated catch block
-		System.out.println(""+e.getMessage());
 	}
 
-	}
 }
-
