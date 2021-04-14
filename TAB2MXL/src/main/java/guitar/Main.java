2eb37a346199f0495a2b5005edfadfd3b37665f1
Abhirami Venugopal <abhirami@my.yorku.ca>
2021-04-14 07:43:24 -0400

    jaxb testing comments removed
    saved on abhi's deskptop as "jaxb testing"

--- TAB2MXL/src/main/java/guitar/Main.java
+++ TAB2MXL/src/main/java/guitar/Main.java
@@ -24,31 +24,6 @@ public static void main(String[] args) {
 		ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 		ms.marshal(part,System.out);	
 		
-//		StaffTuning st = new StaffTuning(1,"A",1);
-//		Key k = new Key(1);
-//		Time t = new Time(4, 4);
-//		Clef c = new Clef("A", 1);
-//		StaffDetails sd = new StaffDetails();
-//		Attributes a = new Attributes(k, t, c, sd);
-//		a.setDivisions(2);
-		
-//		Pitch p = new Pitch ("A", 1, "A"); //
-//		Slide s = new Slide("A", 1, "A"); //
-//		Slur sr = new Slur(1, "A"); // 
-//		Technical t = new Technical(2, 1); //
-//		Chord c = new Chord();
-//		Notations n = new Notations(t, sr, s);
-//		Note n1 = new Note(p, 2, "hello", n);
-//		
-//		
-//		
-//		JAXBContext jc = JAXBContext.newInstance(Note.class);
-//		Marshaller ms = jc.createMarshaller();
-//		ms.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//		ms.marshal(n1,System.out);	
-//		
-		
-
 	}catch (JAXBException ex) {
 		// TODO Auto-generated catch block
 		System.out.println(""+ex.getMessage());
