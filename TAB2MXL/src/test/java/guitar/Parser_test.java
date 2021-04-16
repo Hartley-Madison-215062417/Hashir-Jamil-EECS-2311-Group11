9b451f135aa2b96d47fd7b3ce161f53c26692476
Abhirami Venugopal <abhirami@my.yorku.ca>
2021-04-16 04:52:21 -0400

    more tests written for parser

--- TAB2MXL/src/test/java/guitar/Parser_test.java
+++ TAB2MXL/src/test/java/guitar/Parser_test.java
@@ -63,10 +63,64 @@ public class Parser_test {
 		scorePartwise sp = new scorePartwise();
 		sp.getParts().add(part);
 		
-		System.out.println(p.getPart().getPart().size());
+		//System.out.println(p.getPart().getPart().size());
 		assertNotNull(p.getPart().getPart().get(0).getBarline2().getRepeat());	
 	}
 	
+	@Test
+	void testDoubleDigitFretBasic() {
+		Parser p = new Parser("DoubleFretBasic.txt");
+		char[][] parsed = p.getTabCharMatrix();
+		int width = parsed[0].length; //gets the length of the entire first row
+		ArrayList<char[][]>tmpArray1 = p.measureSplitter(parsed);
+		Part part = p.createMusicalPart(tmpArray1);
+		scorePartwise sp = new scorePartwise();
+		sp.getParts().add(part);
+		assertEquals(12, p.getPart().getPart().get(1).notes.get(4).getNotations().getTechnical().getFret());		
+		assertAll(
+				 "heading",
+			      () -> assertEquals(14, p.getPart().getPart().get(1).notes.get(2).getNotations().getTechnical().getFret() ),
+			      () -> assertEquals(14, p.getPart().getPart().get(1).notes.get(3).getNotations().getTechnical().getFret() ),
+			      () -> assertEquals(12, p.getPart().getPart().get(1).notes.get(4).getNotations().getTechnical().getFret() )
+
+		);
+	}
+	
+	@Test
+	void testSingleDigitFretBasic() {
+		Parser p = new Parser("SingleFretBasic.txt");
+		char[][] parsed = p.getTabCharMatrix();
+		int width = parsed[0].length; //gets the length of the entire first row
+		ArrayList<char[][]>tmpArray1 = p.measureSplitter(parsed);
+		Part part = p.createMusicalPart(tmpArray1);
+		scorePartwise sp = new scorePartwise();
+		sp.getParts().add(part);
+		assertAll(
+				 "heading",
+			      () -> assertEquals(0, p.getPart().getPart().get(0).notes.get(0).getNotations().getTechnical().getFret() ),
+			      () -> assertEquals(4, p.getPart().getPart().get(0).notes.get(1).getNotations().getTechnical().getFret() ),
+			      () -> assertEquals(2, p.getPart().getPart().get(0).notes.get(2).getNotations().getTechnical().getFret() ),
+			      () -> assertEquals(5, p.getPart().getPart().get(0).notes.get(3).getNotations().getTechnical().getFret() )
+
+		);
+	}
+	
+	@Test
+	void testChord() {
+		Parser p = new Parser("ChordBasic.txt");
+		char[][] parsed = p.getTabCharMatrix();
+		int width = parsed[0].length; //gets the length of the entire first row
+		ArrayList<char[][]>tmpArray1 = p.measureSplitter(parsed);
+		Part part = p.createMusicalPart(tmpArray1);
+		scorePartwise sp = new scorePartwise();
+		sp.getParts().add(part);
+		assertNotNull(p.getPart().getPart().get(0).getNotes().get(1).getChord());
+		assertNotNull(p.getPart().getPart().get(0).getNotes().get(2).getChord());
+		assertNotNull(p.getPart().getPart().get(0).getNotes().get(3).getChord());
+		assertNotNull(p.getPart().getPart().get(0).getNotes().get(4).getChord());
+		assertNotNull(p.getPart().getPart().get(0).getNotes().get(5).getChord());
+		
+	}
 	
 
 }
