2b84a1382d548ffc3e9b5746ac9a23f44edba783
Abhirami Venugopal <abhirami@my.yorku.ca>
2021-04-17 14:50:42 -0400

    custom tuning changes

--- TAB2MXL/src/main/java/guitar/StaffDetails.java
+++ TAB2MXL/src/main/java/guitar/StaffDetails.java
@@ -1,5 +1,7 @@
 package guitar;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -43,9 +45,16 @@ public class StaffDetails{
 		s5 = new StaffTuning(5, "B", 3);
 		s6 = new StaffTuning(6, "E", 4);
 		
-		
-		
-		
+	}
+	
+	public StaffDetails(List<String> stringTune) {
+		staffLines = 6;
+		s1 = new StaffTuning(1, stringTune.get(0), 2);
+		s2 = new StaffTuning(2, stringTune.get(1), 2);
+		s3 = new StaffTuning(3, stringTune.get(2), 3);
+		s4 = new StaffTuning(4, stringTune.get(3), 3);
+		s5 = new StaffTuning(5, stringTune.get(4), 3);
+		s6 = new StaffTuning(6, stringTune.get(5), 4);
 	}
 
 	public int getStaffLines() {
